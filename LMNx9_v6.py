55fb8e4a76943590a6de7e4114e0ffcfdf4b5724
DARK_TEAM_LMNx9 <145145650+LMNx9-JOHNY@users.noreply.github.com>
2024-10-21 18:31:45 +0600

    Add files via upload

---
+++ LMNx9_v6.py
@@ -0,0 +1,12 @@
+import os,sys,platform,time
+v6 = platform.architecture()[0]
+if v6 == '64bit':import LMNx9_v6
+elif v6 == '32bit':
+    os.system("clear")
+    os.system('xdg-open https://t.me/TEAM_LMNx9')
+    time.sleep(3)
+    sys.exit("\n</> Your Device Is Not Supported For Run This Tool..!")
+else:
+    os.system("clear")
+    sys.exit("\n</> Something Went Wrong..! Try Again")
+    
