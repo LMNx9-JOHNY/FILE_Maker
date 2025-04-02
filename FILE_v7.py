d301f136342937f6d4bcde994553e9b4f5f17e25
DARK_TEAM_LMNx9 <145145650+LMNx9-JOHNY@users.noreply.github.com>
2025-04-02 18:41:48 +0600

    Add files via upload

---
+++ FILE_v7.py
@@ -0,0 +1,14 @@
+# Fuxk You Kids || - LMNx9
+
+import os,sys,platform,time
+lmnXsukh = platform.architecture()[0]
+if lmnXsukh == '64bit':import FILE_v7
+elif lmnXsukh == '32bit':
+    os.system("clear")
+    os.system('xdg-open https://t.me/TEAM_LMNx9')
+    time.sleep(3)
+    sys.exit("\n</> Your Device Is Not Supported For Run This Tool..!\n")
+else:
+    os.system("clear")
+    sys.exit("\n</> Something Went Wrong..! Try Again\n")
+    
